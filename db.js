553b72d8c3edaf45ec1231e08b9e614e818db779
Umang Gupta <90057524+Umang771@users.noreply.github.com>
2026-06-28 16:57:11 +0530

    Update MongoDB connection URIs in db.js
    Comment out the inotebook connection URI and uncomment the default URI.

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-//const mongoURI = "mongodb+srv://Umang771:Harsh@09@cluster0.xemndtl.mongodb.net/"
-const mongoURI = "mongodb+srv://Umang771:Harsh@09@cluster0.xemndtl.mongodb.net/inotebook?retryWrites=true&w=majority"
+const mongoURI = "mongodb+srv://Umang771:Harsh@09@cluster0.xemndtl.mongodb.net/"
+//const mongoURI = "mongodb+srv://Umang771:Harsh@09@cluster0.xemndtl.mongodb.net/inotebook?retryWrites=true&w=majority"
                                                           
 
 const connectToMongo = async ()=>{
